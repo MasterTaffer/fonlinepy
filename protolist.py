58ddbdaf6df12f83e9916b129ce0b1b8efd1fab5
gorret taffard <odestrzo@gmail.com>
2018-08-26 20:08:02 +0300

    add active and print functionality to protolist.py

--- protolist.py
+++ protolist.py
@@ -29,10 +29,14 @@ if __name__ == "__main__":
 	parser.add_argument("input", type=str)
 	parser.add_argument("--free", type=int)
 	parser.add_argument("--locate", type=int)
+	parser.add_argument("--active-only", action="store_true")
+	parser.add_argument("--print", action="store_true")
 	args = parser.parse_args()
 	input_folder = args.input
 	free_pids = args.free
 	locate = args.locate
+	active_only = args.active_only
+	print_protos = args.print
 	
 	total_protos = 0
 	
@@ -41,10 +45,14 @@ if __name__ == "__main__":
 	
 	protos = protofile.load_freestanding_proto_files(input_folder)
 	for x in protos:
+		if active_only and not x.active:
+			continue
 		x.read_file()
 		x.read_sections()
 		update_count = 0
 		proto_cnt = len(x.sections)
+		if print_protos:
+			print("Proto file {} ({}) containing {} protos".format(x.name, "ACTIVE" if x.active else "INACTIVE", proto_cnt))
 		total_protos += proto_cnt
 		for pid, sect in x.sections.items():
 			fn = x.name
